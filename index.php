3231c4b0fe56f4b1cd2796adfd3157e9daf1c532
Bataino Ronaldo <Bataino.ronaldo@gmail.com>
2020-12-13 15:50:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,30 @@
-<script src="//code.tidio.co/fouwfr0cnygz4sj8kttyv0cz1rpaayva.js" async></script>
-<!-- Swap your javascript code above -->
-<script>
-   (function() {
-    function onTidioChatApiReady() {
-      (function() {
-          window.tidioChatApi.open();
-      })();
-    }
-    if (window.tidioChatApi) {
-      window.tidioChatApi.on("ready", onTidioChatApiReady);
-    } else {
-      document.addEventListener("tidioChat-ready", onTidioChatApiReady);
-    }
-  })();
-</script>
+
+<html>
+  <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body onload="load()">
+    <script>
+      function load(){
+        location.href = '#mobile-widget';
+      }
+    </script>
+    <script src="https://widget-v4.tidiochat.com//1_45_0/static/js/widget.66f097c8c651f346d9e7.js" async></script>
+      <!-- Swap your javascript code above -->
+     <script>
+         (function() {
+          function onTidioChatApiReady() {
+            (function() {
+                window.tidioChatApi.open();
+            })();
+          }
+          if (window.tidioChatApi) {
+            window.tidioChatApi.on("ready", onTidioChatApiReady);
+          } else {
+            document.addEventListener("tidioChat-ready", onTidioChatApiReady);
+          }
+        })();
+      </script>
+  </body>
+</html>
+
