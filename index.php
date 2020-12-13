932ec2d667464ddb4740517e862774b38ff865c1
Bataino Ronaldo <Bataino.ronaldo@gmail.com>
2020-12-13 13:31:48 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<script src="//code.tidio.co/fouwfr0cnygz4sj8kttyv0cz1rpaayva.js" async></script>
+<!-- Swap your javascript code above -->
+<script>
+   (function() {
+    function onTidioChatApiReady() {
+      (function() {
+          window.tidioChatApi.open();
+      })();
+    }
+    if (window.tidioChatApi) {
+      window.tidioChatApi.on("ready", onTidioChatApiReady);
+    } else {
+      document.addEventListener("tidioChat-ready", onTidioChatApiReady);
+    }
+  })();
+</script>
